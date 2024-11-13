c7448523cfab38050e0d01f3e5ba74743ea2e3e8
Edgar Sánchez <edgar@honeymustard.mx>
2024-11-13 14:55:01 -0600

    Creación del proyecto

---
+++ satoritech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  satoritech
+//
+//  Created by Edgar Sánchez on 13/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
